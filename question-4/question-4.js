9271e5b17a79eba6a696e1b89e98c011e83028b2
SofiaEmt <sofia.emteryd@gmail.com>
2021-11-03 14:18:46 +0100

    Question 4, html, scss and java done.

---
+++ question-4/question-4.js
@@ -0,0 +1,44 @@
+
+// Here I make the value of the slider show!
+let slider = document.getElementById("question-4-slider");
+let output = document.getElementById("show-values");
+
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+
+  if (this.value <=100){
+      output.innerHTML = sliderSentences [5]
+  }
+  if (this.value <= 95){
+      output.innerHTML = sliderSentences [4]
+  }
+
+  if (this.value < 75){
+      output.innerHTML = sliderSentences [3]
+  }
+
+  if (this.value < 60) {
+      output.innerHTML = sliderSentences [2]
+  }
+
+  if (this.value < 40){
+    output.innerHTML = sliderSentences[1]
+    }
+ 
+  if (this.value < 20){
+    output.innerHTML = sliderSentences[0]
+    }
+
+  if (this.value < 10){
+    output.innerHTML = sliderSentences[7]
+    }
+}
+
+let sliderSentences = 
+    ["Quite a vibe actually", "Not bothered", "I´m fine", "My day was good before this",  "I want to destroy something", "Where's the nearest bridge?", "Drag slider!", "I actually feel happier now"];
+
+output.innerHTML = sliderSentences [6]; 
+
+
