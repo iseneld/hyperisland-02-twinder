312621d0098870c6db33cfc2413ed2c13a6b4bf0
Michel Iseneld <84731866+iseneld@users.noreply.github.com>
2021-11-04 14:17:52 +0100

    Slider and background functions written.

---
+++ pages/question-7/question-7.js
@@ -0,0 +1,29 @@
+let slider = document.getElementById("question-7-slider");
+let outputSalary = document.getElementById("show-salary");
+let outputAge = document.getElementById("show-age");
+
+function setStyle(item, i) {
+  x = Math.round(slider.value / 400);
+  // Shifts the primary color for each container.
+  hueIncrement = 60;
+  // Offsets the secondary color of the gradient.
+  gradientHueOffset = 100;
+  // Adds 1 to i before multiplying by the hue increment.
+  primaryColor = x;
+  secondaryColor = primaryColor + gradientHueOffset;
+  // Adds inline style to the currently processed container.
+  item.setAttribute(
+    "style",
+    "background-image: linear-gradient(135deg, hsl(" +
+      primaryColor +
+      ", 60%, 83%), hsl(" +
+      secondaryColor +
+      ", 85%, 65%));"
+  );
+}
+
+slider.oninput = function () {
+  outputSalary.innerHTML = this.value;
+  outputAge.innerHTML = Math.round(120 - this.value / 950);
+  setStyle(document.querySelector("body"));
+};
