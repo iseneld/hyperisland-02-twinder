f86534b7e95f2d687915a81e4c26d909091a6796
Michel Iseneld <84731866+iseneld@users.noreply.github.com>
2021-11-04 13:03:18 +0100

    Added comments to the code.

--- scripts/color.js
+++ scripts/color.js
@@ -4,20 +4,24 @@ function gradientMaker(x) {
   divs.forEach(setStyle);
 
   function setStyle(item, i) {
-    // Adds 25 hue to each increment.
+    // Shifts the primary color for each container.
     hueIncrement = 60;
-    z = (x + (i + 1) * hueIncrement) % 255;
-    y = (z + 100) % 255;
-
+    // Offsets the secondary color of the gradient.
+    gradientHueOffset = 100;
+    // Adds 1 to i before multiplying by the hue increment.
+    primaryColor = (x + ++i * hueIncrement) % 255;
+    secondaryColor = (primaryColor + gradientHueOffset) % 255;
+    // Adds inline style to the currently processed container.
     item.setAttribute(
       "style",
       "background-image: linear-gradient(135deg, hsl(" +
-        z +
+        primaryColor +
         ", 60%, 83%), hsl(" +
-        y +
+        secondaryColor +
         ", 85%, 65%));"
     );
   }
 }
 
+// Randomizes a primary hue value.
 gradientMaker(1000 * Math.random());
