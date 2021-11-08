bec1651c8493697cb46049f9569dc8279d69829b
Michel Iseneld <84731866+iseneld@users.noreply.github.com>
2021-11-08 14:42:29 +0100

    Dunno

---
+++ pages/question-3/question-3.js
@@ -0,0 +1,51 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+
+import {
+  getFirestore,
+  doc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+   apiKey: "AIzaSyAzkY6QneE4ipawLKGPm8xMSBc3MRjz1Ew",
+   authDomain: "authentication-eb5d0.firebaseapp.com",
+   projectId: "authentication-eb5d0",
+   storageBucket: "authentication-eb5d0.appspot.com",
+   messagingSenderId: "739211806581",
+   appId: "1:739211806581:web:9c2a3ef7167a3812e4753c",
+   measurementId: "G-RWPJ75JFP3"
+ };
+
+
+
+
+ const app = initializeApp(firebaseConfig);
+ const db = getFirestore(app);
+
+ 
+ 
+document.getElementById("1").onclick = async () =>{
+    //Add a new info into database
+    await setDoc(doc(db, "memeTest", "savedAnswer"), {
+        answerIs: "1"
+    })
+}
+document.getElementById("2").onclick = async () =>{
+    //Add a new info into database
+    await setDoc(doc(db, "memeTest", "savedAnswer"), {
+        answerIs: "2"
+    })
+}
+document.getElementById("3").onclick = async () =>{
+    //Add a new info into database
+    await setDoc(doc(db, "memeTest", "savedAnswer"), {
+        answerIs: "3"
+    })
+}
+document.getElementById("4").onclick = async () =>{
+    //Add a new info into database
+    await setDoc(doc(db, "memeTest", "savedAnswer"), {
+        answerIs: "4"
+    })
+}
