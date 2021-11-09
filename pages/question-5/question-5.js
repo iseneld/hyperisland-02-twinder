8a31b48e6bd26fcfd20b5a39d5340b73006ec324
SofiaEmt <sofia.emteryd@gmail.com>
2021-11-09 21:16:15 +0100

    added js to make link work

--- pages/question-5/question-5.js
+++ pages/question-5/question-5.js
@@ -70,3 +70,21 @@ document.getElementById("4").onclick = async () =>{
         answerIs: "4"
     })
 }
+
+//Linking divs to next page:
+
+document.getElementById("1").addEventListener("click", function() {
+    location.href = '../question-6/question-6.html';
+});
+
+  document.getElementById("2").addEventListener("click", function() {
+    location.href = '../question-6/question-6.html';
+});
+
+  document.getElementById("3").addEventListener("click", function() {
+    location.href = '../question-6/question-6.html';
+});
+
+  document.getElementById("4").addEventListener("click", function() {
+    location.href = '../question-6/question-6.html';
+});
